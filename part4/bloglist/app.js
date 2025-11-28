ef7c835b01fa547b5ed1bcbe57b3b7f6e75b1118
盖若晨 <gairuochen@gairuochendeMacBook-Air.local>
2025-11-28 04:37:15 +0200

    Add part4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,6 +9,8 @@ const loginRouter = require('./controllers/login')
 
 const app = express()
 
+console.log('Running backend with NODE_ENV =', process.env.NODE_ENV)
+
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
@@ -27,6 +29,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
